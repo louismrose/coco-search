fd4b0c2a9ae7b4c6c7a1c1e53957b31d5b5e704c
Louis Rose <louis.rose@york.ac.uk>
2013-06-11 11:33:50 +0100

    Add a bit of style with Bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ CocoSearch::Application.routes.draw do
   
   mount Resque::Server, :at => "/resque"
   
+  root "searches#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
