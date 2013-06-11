82ca6cebbf2fcc5ec37b943752061158dd532b4c
Louis Rose <louis.rose@york.ac.uk>
2013-06-11 10:47:44 +0100

    Mount resque web app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CocoSearch::Application.routes.draw do
+  
+  mount Resque::Server, :at => "/resque"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
